60ba0312cbe2ed6dfc54fbcfd2353c40f7ab13af
yasin <yasinarabiw@gmail.com>
2022-11-27 19:53:28 +0330

    Handle received photos as file(Without Compression

--- main.py
+++ main.py
@@ -30,6 +30,28 @@ def tele_pic(update, context):
     # Save log to database
     database.add_process(update.effective_chat.id)
 
+def tele_file(update, context):
+    # Download user photo
+    user_id = str(update.effective_chat.id)
+    photo_name = f"./tmp/{user_id}.jpg"
+    result_photo_name = f"./tmp/result{user_id}.jpg"
+    file_id = update.message.document.file_id
+    new_file = context.bot.getFile(file_id)
+    new_file.download(photo_name)
+    # Process photo
+    add_fg(photo_name, result_photo_name)
+    # Send result to user
+    photo = open(result_photo_name, 'rb')
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo)
+    photo.close()
+    # Delete user photo and result photo
+    if os.path.exists(photo_name):
+        os.remove(photo_name)
+    if os.path.exists(result_photo_name):
+        os.remove(result_photo_name)
+    # Save log to database
+    database.add_process(update.effective_chat.id)
+
 
 def start(update, context):
     database.start_put(update.effective_chat.id)
@@ -53,13 +75,20 @@ if __name__ == '__main__':
 
     start_handler = CommandHandler("start", start)
     photo_handle = MessageHandler(Filters.photo, tele_pic)
+    file_photo_handler = MessageHandler(Filters.document.category('image'), tele_file)
     ex_handler = MessageHandler(Filters.video, ex_message)
     message_handler = MessageHandler(Filters.text, message)
 
     dp.add_handler(photo_handle)
     dp.add_handler(start_handler)
+    dp.add_handler(file_photo_handler)
     dp.add_handler(ex_handler)
     dp.add_handler(message_handler)
 
     updater.start_polling()
     updater.idle()
+
+
+# TODO: 1. process and send profile image at start
+#       2. Create tmp folder at start if not exists
+#       3. Update README.md
